53dcd5badda57736eb0260b43f78c29b03168eda
ekaterinagp <katya.koriakina@gmail.com>
2020-05-15 19:21:24 +0200

    navbar ready

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,29 +5,90 @@ import Marketplace from "../containers/Marketplace";
 import Groups from "../containers/Groups";
 import Profile from "../containers/Profile";
 import { Route, NavLink, Redirect } from "react-router-dom";
+import { IconContext } from "react-icons";
+import { GiElephant } from "react-icons/gi";
+import { MdSearch } from "react-icons/md";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import profile from "../img/image.jpg";
+import {
+  homeIcon,
+  watchIcon,
+  marketIcon,
+  groupsIcon,
+  messageIcon,
+  notificationIcon,
+} from "../components/icons";
+
 import "./navbar.css";
 
 export default function NavBar() {
   return (
     <>
       <div className="nav">
-        <ul>
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/watch">Watch</NavLink>
-          </li>
-          <li>
-            <NavLink to="/marketplace">Marketplace</NavLink>
-          </li>
-          <li>
-            <NavLink to="/groups">Groups</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-        </ul>
+        <div className="left">
+          <IconContext.Provider value={{ color: "blue", size: "3em" }}>
+            <p className="logo">
+              <GiElephant />
+            </p>
+          </IconContext.Provider>
+          <div>
+            <form>
+              <div className="search-input">
+                {" "}
+                <input type="text" />
+                <IconContext.Provider
+                  value={{ size: "1em", position: "absolute" }}
+                >
+                  <MdSearch />
+                </IconContext.Provider>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="middle">
+          <div>
+            <NavLink to="/home" activeClassName="active">
+              {homeIcon()}
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/watch" activeClassName="active">
+              {watchIcon()}
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/marketplace" activeClassName="active">
+              {marketIcon()}
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/groups" activeClassName="active">
+              {groupsIcon()}
+            </NavLink>
+          </div>
+        </div>
+        <div className="right">
+          <div>
+            <NavLink to="/profile">
+              <img className="profile-img" src={profile} alt="Profile" />
+            </NavLink>
+          </div>
+
+          <div>Ekaterina</div>
+          <div>
+            <div className="messages">{messageIcon()}</div>
+            <div className="chat-counter">1</div>
+          </div>
+          <div>
+            <div className="notification">{notificationIcon()}</div>
+            <div className="notification-counter">1</div>
+          </div>
+          {/* <div>
+            <div className="add">
+              <AiOutlinePlusCircle />
+            </div>
+          </div> */}
+        </div>
       </div>
       <div className="App-intro">
         <Route path="/home" exact component={StartPage} />
