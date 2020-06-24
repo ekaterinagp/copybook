2d45ef6ba52f6502a4db7efeae4c71a5dad51097
ekaterinagp <katya.koriakina@gmail.com>
2020-06-24 21:21:35 +0200

    chat in process

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,9 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:9090";
 
 export default function Contacts(props) {
   const [contacts, setContacts] = useState(props.user.friends);
   const [chatOpened, setChatOpened] = useState(false);
+
+  const [response, setResponse] = useState("");
+  const socket = socketIOClient(ENDPOINT);
+  useEffect(() => {
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+    });
+  }, []);
+
+  // const sendChatMsg = (e) => {
+  //   console.log(e.target.value);
+  //   setTimeout(() => {
+  //     socket.emit("chat", "e.target.value");
+  //   }, 500);
+  // };
+  // const sendChatMsg = (e) => {
+  //   setTimeout(() => {
+  //     console.log("honey bunny 2");
+
+  //     socket.emit("chat", "honey bunny 2");
+  //   }, 5000);
+  // };
+
+  // setTimeout(() => {
+  //   console.log("honey bunny");
+
+  //   socket.emit("chat", "honey bunny");
+  // }, 5000);
 
   const addChatOpen = (contacts) => {
     contacts.forEach((contact) => {
@@ -61,7 +91,7 @@ export default function Contacts(props) {
               </h2>
             </div>
 
-            <div>Chat here</div>
+            <div>{response}</div>
             <input placeholder="Type here" className="chat-type"></input>
           </div>
         </>
--- server/server.js
+++ server/server.js
@@ -2,9 +2,57 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("config");
-
 const app = express();
+const server = require("http").createServer(app);
+const socketIo = require("socket.io");
+// const io = require("socket.io")(server, { origins: "http://localhost:3000" });
+// io.origins("*:*");
+
 const port = process.env.PORT || 9090;
+
+const io = socketIo(server); // < Interesting!
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  // socket.emit("FromAPI", "New client connected");
+  // if (interval) {
+  //   clearInterval(interval);
+  // }
+  // interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    // clearInterval(interval);
+  });
+  socket.on("chat", (msg) => {
+    socket.emit("FromAPI", msg);
+  });
+});
+
+////////////////////////////////// socket
+
+// const tech = io.of("/chat");
+
+// //io - server, socket - client events. namespases are joined by clients by sending a request to the server
+// tech.on("connection", (socket) => {
+//   socket.on("join", (data) => {
+//     socket.join(data.room);
+//     tech.in(data.room).emit("message", `new user joined ${data.room} room`);
+//   });
+//   socket.on("message", (data) => {
+//     console.log(`message: ${data.msg}`);
+//     tech.in(data.room).emit("message", data.msg);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+
+//     tech.emit("message", "user disconnected");
+//   });
+// });
+
+////////////////////////////
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -39,16 +87,20 @@ const postRoutes = require("./routes/posts/posts.js");
 const userRoutes = require("./routes/users/users.js");
 const groupsRoutes = require("./routes/groups/groups.js");
 const watchRoutes = require("./routes/watch/watch.js");
+const chatRoutes = require("./routes/chats/chats.js");
 // app.post("/posts", postRoutes);
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 app.use("/groups", groupsRoutes);
 app.use("/watch", watchRoutes);
+app.use("/chats", chatRoutes);
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log("Server error");
-    return;
-  }
-  console.log(`server is listening to port ${port}`);
-});
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log("Server error");
+//     return;
+//   }
+//   console.log(`server is listening to port ${port}`);
+// });
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
